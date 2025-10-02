bbad2b4b0eb94aa1129a48e52392e5f2ab1664d8
Fernanda da Silva Pereira <20221204010005@ifrn.local>
2025-10-02 10:17:12 -0300

    Inicializando repositorio

---
+++ src/main/java/ifrn/pi/eventos/models/Eventoo.java
@@ -0,0 +1,42 @@
+package ifrn.pi.eventos.models;
+
+public class Eventoo {
+
+	private String nome;
+	private string local;
+	private string data;
+	private tring horario;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public string getLocal() {
+		return local;
+	}
+
+	public void setLocal(string local) {
+		this.local = local;
+	}
+
+	public string getData() {
+		return data;
+	}
+
+	public void setData(string data) {
+		this.data = data;
+	}
+
+	public tring getHorario() {
+		return horario;
+	}
+
+	public void setHorario(tring horario) {
+		this.horario = horario;
+	}
+
+}
